f4a06f60aad981a9d9c2f605385b204eff029e47
celorio <ray6989@gmail.com>
2013-10-11 15:15:11 +0300

    Modified server.js to serve static content

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
+var path = require('path');
 var dir = require('./routes/directory');
 
 var app = new express();
+
+app.configure( function() {
+
+	// parses request body and populates request.body
+	app.use( express.bodyParser() );
+
+	// checks request.body for HTTP method overrides
+	app.use( express.methodOverride() );
+
+	// serve the static content
+	app.use ( express.static( path.join( __dirname, 'backbone' )));
+
+	// show errors
+	app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.get('/employees/all', dir.findAll);
 app.get('/employees/search/', dir.findByName);
